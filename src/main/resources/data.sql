3dcb41516b7f148451439f8c97438994b81fbfbf
ulbr <bara0803bara@gmail.com>
2023-06-09 15:34:35 +0300

    Autocomplete users and categories in db

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+INSERT INTO Categories (name) VALUES
+('Application & Services'),
+('Benefits & Paper Work'),
+('Hardware & Software'),
+('People Management'),
+('Security & Access'),
+('Workplaces & Facilities');
+INSERT INTO users (first_name, last_name, role, email, password) VALUES
+('owner1', 'owner1', 'OWNER', 'user1_mogilev@yopmail.com', '$2a$10$hX100lVRPiAEyO5znOhv0uByX/oWn1jn0WDWMV2mh78QEUWyBdYi6'),
+('owner2', 'owner2', 'OWNER', 'user2_mogilev@yopmail.com', '$2a$10$n4rVuFpenHQBgwW3RHQW1OplglhBaq2ikVOcCXF2TT406trUw9qOS'),
+('manager', 'manager', 'MANAGER', 'manager1_mogilev@yopmail.com', '$2a$10$WMlyRC3UXR9N5mtXGt27H.U6SXWcCM7ZUEOeSyW21.X99eB58LbvO'),
+('manager', 'manager', 'MANAGER', 'manager2_mogilev@yopmail.com', '$2a$10$aDgw3sv6FWRBXRjhBfZDButw4MqNEennLbSoGBbdRBqrH2lldXvfW'),
+('engineer1', 'engineer1', 'ENGINEER', 'engineer1_mogilev@yopmail.com', '$2a$10$z5Lspfiw/p4JMuhkvrxO1ekQpLskH4NHOXAUP8H2e6rxwt0Tp.9Om'),
+('engineer2', 'engineer2', 'ENGINEER', 'engineer2_mogilev@yopmail.com', '$2a$10$JpZ62ub4.pCh0Rubf..69Oy4wW0mLUEQzsqMFVd54LzhC/SBMg3CC');
