743b1a1093e28b9ca0100a26071d891661e9b8d8
ulbr <bara0803bara@gmail.com>
2023-06-14 15:55:19 +0300

    Implemented sorting over ticket, added specifications

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -12,3 +12,8 @@ INSERT INTO users (first_name, last_name, role, email, password) VALUES
 ('manager', 'manager', 'MANAGER', 'manager2_mogilev@yopmail.com', '$2a$10$aDgw3sv6FWRBXRjhBfZDButw4MqNEennLbSoGBbdRBqrH2lldXvfW'),
 ('engineer1', 'engineer1', 'ENGINEER', 'engineer1_mogilev@yopmail.com', '$2a$10$z5Lspfiw/p4JMuhkvrxO1ekQpLskH4NHOXAUP8H2e6rxwt0Tp.9Om'),
 ('engineer2', 'engineer2', 'ENGINEER', 'engineer2_mogilev@yopmail.com', '$2a$10$JpZ62ub4.pCh0Rubf..69Oy4wW0mLUEQzsqMFVd54LzhC/SBMg3CC');
+INSERT INTO tickets (name, urgency, urgency_number, created_on, desired_resolution_date, owner_id, state, category_id, approver_id) VALUES
+('name1', 'CRITICAL', 4 , '2023-06-13', '2023-06-23', 1, 'NEW', 1, null),
+('name2', 'AVERAGE', 2, '2023-06-13', '2023-06-16', 2, 'IN_PROGRESS', 1, 3),
+('name3', 'LOW', 1, '2023-06-13', '2023-06-19', 2, 'APPROVED', 1, 3),
+('name4', 'HIGH', 3, '2023-06-13', '2023-06-17', 1, 'CANCELED', 1, null);
