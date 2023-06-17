1ef0426f1cb44bb9f8bb0b6c45177843ea28c78c
ulbr <bara0803bara@gmail.com>
2023-06-17 10:24:35 +0300

    Code refactored

--- src/main/java/com/bara/helpdesk/service/TicketService.java
+++ src/main/java/com/bara/helpdesk/service/TicketService.java
@@ -3,6 +3,7 @@ package com.bara.helpdesk.service;
 import com.bara.helpdesk.dto.*;
 import com.bara.helpdesk.entity.Ticket;
 import com.bara.helpdesk.security.CustomUserDetails;
+import jakarta.transaction.Transactional;
 
 import java.util.List;
 
@@ -20,5 +21,6 @@ public interface TicketService {
 
     TicketOutputDto updateTicket(TicketEditDto dto);
 
-    TicketOutputDto changeTicketState(TicketStateChangeDto dto, Long userId);
+    @Transactional
+    TicketOutputDto changeTicketState(TicketStateChangeDto dto, CustomUserDetails userDetails);
 }
